6fbbd0a1d33cbf20bc4b1e7b66c044a02b5d786a
Volodymyr K <vladimir.k@halo-lab.team>
2024-09-23 19:23:23 +0300

    card cover fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,9 +8,9 @@ type CardP = {
 
 const Card:FC<CardP> = ({title = '', description = '', imgSrc = ''}) => {
   return (
-    <div className="flex flex-col rounded-lg border overflow-hidden shadow-lg h-[560px]" >
-      {!!imgSrc && <div className="flex-1 relative transition-opacity duration-300 ease-in-out hover:opacity-75 overflow-hidden" >
-        <img alt={title} src={imgSrc}   className="absolute inset-0 self-center"/>
+    <div className="flex flex-col rounded-lg border overflow-hidden shadow-lg h-[460px] md:h-[560px]" >
+      {!!imgSrc && <div className="flex-1 transition-opacity duration-300 ease-in-out hover:opacity-75 overflow-hidden" >
+        <img alt={title} src={imgSrc}   className="h-full w-full object-cover"/>
       </div>}
       <div className="p-4 grid gap-2">
         {!!title && <h2 className="text-xl font-semibold text-gray-900">{title}</h2>}
